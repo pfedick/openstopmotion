ca53ff07d7e8b5ed350267b17b59dd7653e3d933
patrick <patrick@e1bf1d8d-f32d-e511-a839-448a5b2c3dd9>
2012-06-07 16:48:46 +0000

    Eigener Slider

--- video/StopMoCap/trunk/myslider.cpp
+++ video/StopMoCap/trunk/myslider.cpp
@@ -4,9 +4,76 @@ MySlider::MySlider(QWidget *parent)
     : QWidget(parent)
 {
 	ui.setupUi(this);
+	noRecursion=false;
+}
+
+MySlider::MySlider(Qt::Orientation orientation, QWidget *parent)
+    : QWidget(parent)
+{
+	ui.setupUi(this);
+	ui.slider->setOrientation(orientation);
+	ui.spinBox->setValue(ui.slider->value());
+	ui.spinBox->setMaximum(ui.slider->maximum());
+	ui.spinBox->setMinimum(ui.slider->minimum());
+	noRecursion=false;
 }
 
 MySlider::~MySlider()
 {
 
+}
+
+
+void MySlider::setMinimum(int value)
+{
+	ui.slider->setMinimum(value);
+	ui.spinBox->setMinimum(value);
+}
+
+void MySlider::setMaximum(int value)
+{
+	ui.slider->setMaximum(value);
+	ui.spinBox->setMaximum(value);
+
+}
+
+void MySlider::setSingleStep(int value)
+{
+	ui.slider->setSingleStep(value);
+}
+
+void MySlider::setPageStep(int value)
+{
+	ui.slider->setPageStep(value);
+}
+
+
+void MySlider::setSliderPosition(int value)
+{
+	ui.slider->setSliderPosition(value);
+	ui.spinBox->setValue(value);
+}
+
+int MySlider::value() const
+{
+	return ui.slider->value();
+}
+
+void MySlider::on_slider_valueChanged ( int value )
+{
+	if (noRecursion) return;
+	printf ("Slider Value changed: %i\n",value);
+	noRecursion=true;
+	ui.spinBox->setValue(value);
+	noRecursion=false;
+
+}
+
+void MySlider::on_spinBox_valueChanged ( int value )
+{
+	if (noRecursion) return;
+	printf ("spinBox Value changed: %i\n",value);
+	noRecursion=true;
+	ui.slider->setValue(value);
+	noRecursion=false;
 }
--- video/StopMoCap/trunk/myslider.h
+++ video/StopMoCap/trunk/myslider.h
@@ -3,6 +3,8 @@
 
 #include <QtGui/QWidget>
 #include <QSlider>
+#include <QSizePolicy>
+#include "device.h"
 #include "ui_myslider.h"
 
 class MySlider : public QWidget
@@ -14,11 +16,24 @@ public:
     MySlider(Qt::Orientation orientation, QWidget *parent = 0);
     ~MySlider();
 
+    void setMinimum(int value);
+    void setMaximum(int value);
+    void setSingleStep(int value);
+    void setPageStep(int value);
+    void setSliderPosition(int value);
+
+    int value() const;
+
     CameraControl cont;
     int lastValue;
 
 private:
     Ui::MySliderClass ui;
+    bool noRecursion;
+
+public slots:
+	void on_slider_valueChanged ( int value );
+	void on_spinBox_valueChanged ( int value );
 };
 
 #endif // MYSLIDER_H
