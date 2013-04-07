060592c69685d2540bc9d4abc48874db1de56371
pafe1234@gmail.com <pafe1234@gmail.com@e1bf1d8d-f32d-e511-a839-448a5b2c3dd9>
2013-04-07 18:29:30 +0000

    Lizenz-Informationen

--- OpenStopMotion/trunk/src/FrameBuffer.cpp
+++ OpenStopMotion/trunk/src/FrameBuffer.cpp
@@ -1,9 +1,28 @@
 /*
- * FrameBuffer.cpp
+ * This file is part of OpenStopMotion by Patrick Fedick
  *
- *  Created on: 08.09.2012
- *      Author: patrick
+ * $Author$
+ * $Revision$
+ * $Date$
+ * $Id$
+ *
+ *
+ * Copyright (c) 2013 Patrick Fedick
+ *
+ * This program is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
+
 
 #include "FrameBuffer.h"
 
